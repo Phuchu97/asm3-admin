6c73beffcf51aaca0dc01d354c935d398485366a
cvm <vanmanh3c9@gmail.com>
2025-09-28 15:34:59 +0700

    fix: correct api url

--- src/Constants/ApiConstant.js
+++ src/Constants/ApiConstant.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4000';
-// export const API_URL = 'https://vuongphatsteel.com/api';
+// export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://vuongphatsteel.com/api';
